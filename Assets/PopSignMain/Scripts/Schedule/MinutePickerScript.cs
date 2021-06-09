1176ca1f653941eb5b459ad03307e98b17c13b45
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2021-06-09 16:46:28 -0400

    pupolate the picker

--- Assets/PopSignMain/Scripts/Schedule/MinutePickerScript.cs
+++ Assets/PopSignMain/Scripts/Schedule/MinutePickerScript.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HourPickerScript : MonoBehaviour
+public class MinutePickerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Text textPrefab;
     public GameObject content;
     public GameObject pickerFrame;
@@ -13,10 +12,10 @@ public class HourPickerScript : MonoBehaviour
 
     void Start()
     {
-        for (int i = 11; i <= 26; i++)
+        for (int i = 58; i <= 121; i++)
         {
             Text text = Text.Instantiate(textPrefab);
-            text.text = (i % 12 == 0? 12: i % 12) + "";
+            text.text = (i % 60 < 10? "0": "") + (i % 60);
             text.transform.parent = content.transform;
             choices.Add(text);
         }
@@ -25,6 +24,6 @@ public class HourPickerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
--- Assets/PopSignMain/Scripts/Schedule/TimeOfDayPickerScript.cs
+++ Assets/PopSignMain/Scripts/Schedule/TimeOfDayPickerScript.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HourPickerScript : MonoBehaviour
+public class TimeOfDayPickerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
     public Text textPrefab;
     public GameObject content;
     public GameObject pickerFrame;
@@ -13,12 +12,21 @@ public class HourPickerScript : MonoBehaviour
 
     void Start()
     {
-        for (int i = 11; i <= 26; i++)
+        for (int i = 1; i <= 6; i++)
         {
             Text text = Text.Instantiate(textPrefab);
-            text.text = (i % 12 == 0? 12: i % 12) + "";
+            text.text = "";
+            if (i == 3)
+            {
+                text.text = "AM";
+                choices.Add(text);
+            }
+            if (i == 4)
+            {
+                text.text = "PM";
+                choices.Add(text);
+            }
             text.transform.parent = content.transform;
-            choices.Add(text);
         }
     }
 
