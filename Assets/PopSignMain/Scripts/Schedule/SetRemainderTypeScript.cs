2197da39baa8824a2f6c2d4d689236819290d40e
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2021-06-09 17:10:41 -0400

    pick remainder type

---
+++ Assets/PopSignMain/Scripts/Schedule/SetRemainderTypeScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SetRemainderTypeScript : MonoBehaviour
+{
+    public GameObject dailyEditor, weeklyEditor;
+    public Dropdown target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onRemainderTypeChanged()
+    {
+        if (target.value == 0)
+        {
+            dailyEditor.SetActive(false);
+            weeklyEditor.SetActive(false);
+        }
+        else if (target.value == 1)
+        {
+            dailyEditor.SetActive(true);
+            weeklyEditor.SetActive(false);
+        }
+        else if (target.value == 2)
+        {
+            dailyEditor.SetActive(true);
+            weeklyEditor.SetActive(true);
+        }
+    }
+}
