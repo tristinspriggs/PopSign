f0261f36155b9113964d26be7c0b65356e3a25bc
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2021-06-21 17:27:39 -0400

    save schedule handler declaration

---
+++ Assets/PopSignMain/Scripts/CustomizeLevelScript/SaveScheduleHandler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveScheduleHandler : MonoBehaviour
+{
+    public Dropdown scheduleType;
+    public Text scheduledTime;
+    public Toggle[] scheduledDay;
+    public OpenScheduleScene closeObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void saveSchedule()
+    {
+
+    }
+}
