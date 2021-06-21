4353164cde13872b7455d03157010e2dc4dda6cc
LechenDai43 <46640649+LechenDai43@users.noreply.github.com>
2021-06-21 18:11:04 -0400

    save button done

--- Assets/PopSignMain/Scripts/CustomizeLevelScript/SaveScheduleHandler.cs
+++ Assets/PopSignMain/Scripts/CustomizeLevelScript/SaveScheduleHandler.cs
@@ -23,6 +23,56 @@ public class SaveScheduleHandler : MonoBehaviour
 
     public void saveSchedule()
     {
+        int typeIndex = scheduleType.value;
 
+        if (typeIndex == 0)
+        {
+            PlayerPrefs.SetString("Schedule Type", "None");
+            // Delete notifications
+        }
+        else if (typeIndex == 1)
+        {
+            PlayerPrefs.SetString("Schedule Type", "Daily");
+            parseTime();      
+            // Set repeatable notifications for everyday
+        }
+        else
+        {
+            PlayerPrefs.SetString("Schedule Type", "Weekly");
+            parseTime();
+            string[] dayOfWeek = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            for (int i = 0; i < 7; i++)
+            {
+                Toggle theToggle = scheduledDay[i];
+                string theDay = dayOfWeek[i];
+
+                if (theToggle.isOn)
+                {
+                    PlayerPrefs.SetInt(theDay, 1);
+                }
+                else
+                {
+                    PlayerPrefs.SetInt(theDay, 0);
+                }
+            }
+            // Set weekly notification
+        }
+
+        closeObject.exitScheduleScene();
+
+    }
+
+    private void parseTime()
+    {
+        string rawString = scheduledTime.text;
+        int hour = int.Parse(rawString.Substring(0, rawString.IndexOf(":")));
+        int minute = int.Parse(rawString.Substring(rawString.IndexOf(":") + 1, rawString.IndexOf(" ") - 2));
+        hour = hour == 12 ? 0 : hour;
+        hour = rawString.Contains("AM") ? hour : hour + 12;
+
+        Debug.Log(hour);
+        Debug.Log(minute);
+        PlayerPrefs.SetInt("Hour", hour);
+        PlayerPrefs.SetInt("Minute", minute);
     }
 }
